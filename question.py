636dc823e26513a7ceecbb8303c9db4e5803245d
petergla <petergla@buffalo.edu>
2017-06-13 10:34:52 -0400

    Commented code

--- question.py
+++ question.py
@@ -3,18 +3,16 @@ class Question:
     def __init__(self, name, rw_model, ideal_model):
         self.name = name
         self.assumptions = {"true" : [], "false" : [], "irrelevant" :[]}
-        self.rw_model = rw_model
-        self.ideal_model = ideal_model
+        self.rw_model = rw_model #String of path to real world model image
+        self.ideal_model = ideal_model #String of path to idealized model image
 
-
-    # Truth values for asumptions represented as strings 
+    # Author: PJ Glasheen
+    # Allows assumptions to be entered into the question class. Truth values represented as strings.
     def add_assumption(self, truth_value, assumption):
         if truth_value == "true" or truth_value == "false" or truth_value == "irrelevant":
             self.assumptions[truth_value].append(assumption)
         else:
             print "Invalid truth value"
-
-
 
     # Author: Paata Ugrekhelidze
     # Allows to get all the values from the data depending what is requested
@@ -29,4 +27,3 @@ class Question:
             return self.rw_model
         else:
             print "Invalid Input"
-        
