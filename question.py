bffe27761bad22f9f6520f03abaf2b2109c66ef3
petergla <petergla@buffalo.edu>
2017-06-13 11:49:49 -0400

    Created new classes & edited question class
    Created Enum class TruthValue and class for assumptions. Changed
    question class to utilize these classes

--- question.py
+++ question.py
@@ -1,18 +1,19 @@
+from truthvalue import TruthValue
+from assumption import Assumption
 class Question:
 
     def __init__(self, name, rw_model, ideal_model):
         self.name = name
-        self.assumptions = {"true" : [], "false" : [], "irrelevant" :[]}
+        self.assumptions = []
         self.rw_model = rw_model #String of path to real world model image
         self.ideal_model = ideal_model #String of path to idealized model image
 
     # Author: PJ Glasheen
-    # Allows assumptions to be entered into the question class. Truth values represented as strings.
-    def add_assumption(self, truth_value, assumption):
-        if truth_value == "true" or truth_value == "false" or truth_value == "irrelevant":
-            self.assumptions[truth_value].append(assumption)
-        else:
-            print "Invalid truth value"
+    # Allows assumptions to be entered into the question class.
+    def add_assumption(self,truth_value, assumption_text, reason_list):
+        newAssumption = Assumption(truth_value, assumption_text, reason_list)
+        self.assumptions.append(newAssumption)
+        print self.assumptions
 
     # Author: Paata Ugrekhelidze
     # Allows to get all the values from the data depending what is requested
---
+++ truthvalue.py
@@ -0,0 +1,6 @@
+# Author: PJ Glasheen
+# Enum class for truth values of assumptions
+class TruthValue:
+    false = 0
+    true = 1
+    irrelevant = 3
