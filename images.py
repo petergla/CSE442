23b27ab661dd080fc63955f9563a48040b2ee59b
wahla68 <Wahla68@gmail.com>
2017-06-13 16:31:17 -0400

    code to project an image
    the image has to be in .GIF format and has to be in the same directory as the main for this code to work.

---
+++ images.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+
+root = Tk()
+
+imgPath = "RealWorld1.gif"
+photo = PhotoImage(file = imgPath)
+label = Label(image = photo)
+label.image = photo # keep a reference!
+label.grid(row = 3, column = 1, padx = 5, pady = 5)
+
+root.mainloop()
