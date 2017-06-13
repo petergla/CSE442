c4b142005ab4a695ba392a97708214213d805bf6
wahla68 <Wahla68@gmail.com>
2017-06-13 16:47:59 -0400

    comment

--- images.py
+++ images.py
@@ -2,8 +2,8 @@ from Tkinter import *
 
 root = Tk()
 
-imgPath = "RealWorld1.gif"
-photo = PhotoImage(file = imgPath)
+imgPath = "RealWorld1.gif"  #file name 
+photo = PhotoImage(file = imgPath) 
 label = Label(image = photo)
 label.image = photo # keep a reference!
 label.grid(row = 3, column = 1, padx = 5, pady = 5)
